247b40946acc9ef54f6be178720bf1b19aa50684
zabir emu <zabirraihan570@gmail.com>
2024-05-27 17:02:58 +0600

    add routes and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SingUp from "./pages/SingUp";
 
 const App = () => {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sing-up" element={<SingUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
